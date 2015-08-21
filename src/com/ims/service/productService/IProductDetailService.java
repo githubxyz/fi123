b105d83d38f4bb958447cf634adbb039ff97125b
githubxyz <githubxyz@gmail.com>
2015-08-21 18:45:10 +0530

    rm

---
+++ src/com/ims/service/productService/IProductDetailService.java
@@ -0,0 +1,11 @@
+package com.ims.service.productService;
+
+import java.util.List;
+
+import com.ims.dto.ProductDetailDTO;
+import com.ims.dto.ProductMasterDTO;
+import com.ims.exception.OperationFailedException;
+
+public interface IProductDetailService {
+	public List<ProductDetailDTO> listOfPurchase()throws OperationFailedException;
+}
---
+++ src/com/ims/service/productService/ProductDetailServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ims.service.productService;
+
+import java.util.List;
+
+import com.ims.dto.ProductDetailDTO;
+import com.ims.exception.OperationFailedException;
+
+public class ProductDetailServiceImpl implements IProductDetailService {
+
+	@Override
+	public List<ProductDetailDTO> listOfPurchase() throws OperationFailedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
